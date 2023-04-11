a51ac08199ef867616d9da21f9b8d90b71ec54ee
MD-ASHARUDDIN-90 <asharansari90@gmail.com>
2023-04-11 15:45:46 +0530

    profile avtar aded

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -24,7 +24,7 @@ export default function About() {
         </ul>
         </div>
         <div className={style.image}>
-        <img className={style.imagePic} src="https://imgur.com/jEdtqEr.jpg" alt="profile pic"/>
+        <img className={style.imagePic} src="https://camo.githubusercontent.com/8bf6f6d78abc81fcf9c49f10649423e73ea44bc248e83aaae8759d401c829a84/68747470733a2f2f70687973696373677572756b756c2e66696c65732e776f726470726573732e636f6d2f323031392f30322f6368617261637465722d312e676966" alt="profile pic"/>
         </div>
       </div>
     </>
